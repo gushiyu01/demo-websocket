ccec2de9ca5e05a7637294f094564a6496543b2d
gushiyu <122600gg>
2020-05-28 16:57:36 +0800

    Initial commit

---
+++ src/main/java/com/example/websocket/DemoWebsocketApplication.java
@@ -0,0 +1,18 @@
+package com.example.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Create by gsy
+ * @Date: Create in 2020/5/28 16:53
+ * @Description: 
+ */
+@SpringBootApplication
+public class DemoWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (DemoWebsocketApplication.class, args);
+    }
+
+}
